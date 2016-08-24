428fb1b41743d0493ec56a98ed5f2f452337781c
Vedran Pavic <vedran.pavic@gmail.com>
2016-08-24 21:33:42 +0200

    Initial commit

---
+++ src/main/java/com/example/security/web/authentication/ExpiredCredentialsRedirectFilter.java
@@ -0,0 +1,63 @@
+package com.example.security.web.authentication;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.util.UrlUtils;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.util.Assert;
+import org.springframework.web.filter.GenericFilterBean;
+
+public class ExpiredCredentialsRedirectFilter extends GenericFilterBean {
+
+	private String expiredCredentialsUrl;
+
+	private RequestMatcher requestMatcher;
+
+	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+	public ExpiredCredentialsRedirectFilter(String expiredCredentialsUrl) {
+		Assert.hasLength(expiredCredentialsUrl, "expiredCredentialsUrl must be specified");
+		Assert.isTrue(UrlUtils.isValidRedirectUrl(expiredCredentialsUrl),
+				expiredCredentialsUrl + " isn't a valid redirect URL");
+		this.expiredCredentialsUrl = expiredCredentialsUrl;
+		this.requestMatcher = new RegexRequestMatcher(expiredCredentialsUrl, null);
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+
+		if (!this.requestMatcher.matches(request)) {
+			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+			if (authentication != null && credentialsExpired(authentication)) {
+				this.redirectStrategy.sendRedirect(request, response, this.expiredCredentialsUrl);
+			}
+		}
+
+		chain.doFilter(request, response);
+	}
+
+	protected boolean credentialsExpired(Authentication authentication) {
+		if (authentication.getPrincipal() instanceof UserDetails) {
+			UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+			return !userDetails.isCredentialsNonExpired();
+		}
+		return false;
+	}
+
+}
