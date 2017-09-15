cebe1167aae5c19b04374fd48713ee8c9d691cdd
Vedran Pavic <vedran.pavic@gmail.com>
2017-09-15 08:14:52 +0200

    Add support for forwarding

--- src/main/java/com/example/security/web/authentication/ExpiredCredentialsRedirectFilter.java
+++ src/main/java/com/example/security/web/authentication/ExpiredCredentialsRedirectFilter.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -18,9 +16,11 @@ import org.springframework.security.web.util.UrlUtils;
 import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.util.Assert;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-public class ExpiredCredentialsRedirectFilter extends GenericFilterBean {
+public class ExpiredCredentialsRedirectFilter extends OncePerRequestFilter {
+
+	public static final String SAVED_URL_ATTRIBUTE = ExpiredCredentialsRedirectFilter.class.getName() + ".SAVED_URL";
 
 	private final String expiredCredentialsUrl;
 
@@ -28,28 +28,42 @@ public class ExpiredCredentialsRedirectFilter extends GenericFilterBean {
 
 	private final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
+	private boolean useForward;
+
 	public ExpiredCredentialsRedirectFilter(String expiredCredentialsUrl) {
-		Assert.hasLength(expiredCredentialsUrl, "expiredCredentialsUrl must not be empty");
 		Assert.isTrue(UrlUtils.isValidRedirectUrl(expiredCredentialsUrl),
 				expiredCredentialsUrl + " is not a valid redirect URL");
 		this.expiredCredentialsUrl = expiredCredentialsUrl;
 		this.requestMatcher = new RegexRequestMatcher(expiredCredentialsUrl.replace("?", "\\?"), null);
 	}
 
-	@Override
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-			throws IOException, ServletException {
-		HttpServletRequest request = (HttpServletRequest) req;
-		HttpServletResponse response = (HttpServletResponse) res;
+	public ExpiredCredentialsRedirectFilter(String expiredCredentialsUrl, boolean useForward) {
+		this(expiredCredentialsUrl);
+		this.useForward = useForward;
+	}
 
+	@Override
+	protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
 		if (!this.requestMatcher.matches(request)) {
 			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 			if (authentication != null && credentialsExpired(authentication)) {
-				this.redirectStrategy.sendRedirect(request, response, this.expiredCredentialsUrl);
+				return false;
 			}
 		}
+		return true;
+	}
 
-		chain.doFilter(request, response);
+	@Override
+	public void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+			FilterChain filterChain) throws IOException, ServletException {
+		request.getSession().setAttribute(SAVED_URL_ATTRIBUTE, UrlUtils.buildFullRequestUrl(request));
+		if (this.useForward) {
+			request.getRequestDispatcher(this.expiredCredentialsUrl).forward(request, response);
+		}
+		else {
+			this.redirectStrategy.sendRedirect(request, response, this.expiredCredentialsUrl);
+			filterChain.doFilter(request, response);
+		}
 	}
 
 	protected boolean credentialsExpired(Authentication authentication) {
